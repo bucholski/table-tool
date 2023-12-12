e1cfe5ce55927c2b8085279f8a776a18226736c3
bucholski <bucholskim@gmail.com>
2023-12-12 20:41:31 +0100

    Refactor, finish parsing both ways

--- src-tauri/src/structure.rs
+++ src-tauri/src/structure.rs
@@ -1,3 +1,6 @@
+//TODO
+//every From method should return a Result e.g. Result<Ok(Cell), Err(e)>
+
 #[derive(Debug, PartialEq)]
 pub struct Cell {
     pub x: u32,
@@ -7,65 +10,90 @@ pub struct Cell {
     pub data: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Row {
     pub order: u32,
     pub cells: Vec<Cell>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Table {
     pub rows: Vec<Row>,
 }
 
-trait Parse {
+// pub struct HTLM {
+//     pub data: String,
+// }
+
+pub trait ToHtml {
     fn to_html(&self) -> String;
 }
 
-impl From<String> for Cell {
-    fn from(html_cell: String) -> Cell {
-        let data_start = html_cell.find("<input>").unwrap();
-        let data_end = html_cell.find("</input>").unwrap();
-        let data_string: String = html_cell
-            .clone()
-            .drain(data_start..data_end)
-            .collect::<String>()
-            .trim_start_matches("<input>")
-            .to_string();
-
-        let data = match data_string.is_empty() {
-            true => None,
-            false => Some(data_string),
-        };
-
-        let cell_split: Vec<&str> = html_cell
-            .trim_end_matches("></input></td>")
-            .trim_start_matches("<td ")
-            .split('=')
+impl From<&String> for Cell {
+    fn from(html_cell: &String) -> Cell {
+        let values: Vec<String> = html_cell
+            .trim_start_matches("<td x=")
+            .replace(" y=", ",")
+            .replace(" rowspan=", ",")
+            .replace(" colspan=", ",")
+            .replace(" ><input>", ",")
+            .trim_end_matches("</input></td>")
+            .split(',')
+            .map(|x| x.to_string())
             .collect();
-        let cell_split_2 = &cell_split
-            .iter()
-            .flat_map(|x| x.split(' '))
-            .collect::<Vec<&str>>();
-        let x = cell_split_2[1].parse().unwrap();
-        let y = cell_split_2[3].parse().unwrap();
-        let rowspan = match cell_split_2[5].parse() {
-            Ok(a) => Some(a),
-            Err(e) => {
-                dbg!(e);
-                None
-            }
-        };
-        let colspan = match cell_split_2[7].parse() {
-            Ok(a) => Some(a),
-            Err(e) => None,
-        };
-
-        new_cell(x, y, rowspan, colspan, data)
+        new_cell(
+            values[0]
+                .parse()
+                .unwrap_or(panic!("Value of x failed when paring html cell to struct")),
+            values[1]
+                .parse()
+                .unwrap_or(panic!("Value of y failed when paring html cell to struct")),
+            values[2].parse().unwrap_or(panic!(
+                "Value of colspan failed when paring html cell to struct"
+            )),
+            values[3].parse().unwrap_or(panic!(
+                "Value of rowspan failed when paring html cell to struct"
+            )),
+            values[4].to_string(),
+        )
     }
 }
 
-impl Parse for Cell {
+impl From<&String> for Row {
+    fn from(html_row: &String) -> Row {
+        let html_cells: Vec<&str> = html_row
+            .trim_start_matches("<tr>")
+            .trim_end_matches("</tr>")
+            .split_inclusive("</td>")
+            .collect();
+        let cells: Vec<Cell> = html_cells
+            .iter()
+            .map(|html_cell| Cell::from(&html_cell.to_string()))
+            .collect();
+        Row {
+            order: cells[0].y,
+            cells,
+        }
+    }
+}
+
+impl From<&String> for Table {
+    fn from(html_table: &String) -> Table {
+        let html_rows: Vec<&str> = html_table
+            .trim_start_matches("<table>")
+            .trim_end_matches("</table>")
+            .split_inclusive("</tr>")
+            .collect();
+        let rows: Vec<Row> = html_rows
+            .iter()
+            .map(|html_row| Row::from(&html_row.to_string()))
+            .collect();
+
+        Table { rows }
+    }
+}
+
+impl ToHtml for Cell {
     fn to_html(&self) -> String {
         format!(
             "<td x={} y={} rowspan={} colspan={} ><input>{}</input></td>",
@@ -74,23 +102,7 @@ impl Parse for Cell {
     }
 }
 
-impl From<String> for Row {
-    fn from(html_row: String) -> Row {
-        let html_cells: Vec<&str> = html_row
-            .trim_start_matches("<tr>")
-            .trim_end_matches("</tr>")
-            .split_inclusive("</td>")
-            .collect();
-        let cells: Vec<Cell> = html_cells
-            .iter()
-            .map(|html_cell| Cell::from(html_cell.to_string()))
-            .collect();
-        //finish after TODO above
-        new_row(3, 1)
-    }
-}
-
-impl Parse for Row {
+impl ToHtml for Row {
     fn to_html(&self) -> String {
         let html_cells: String = self
             .cells
@@ -101,7 +113,7 @@ impl Parse for Row {
     }
 }
 
-impl Parse for Table {
+impl ToHtml for Table {
     fn to_html(&self) -> String {
         let html_rows: String = self
             .rows
@@ -112,37 +124,21 @@ impl Parse for Table {
     }
 }
 
-pub fn new_cell(
-    x: u32,
-    y: u32,
-    rowspan: Option<u32>,
-    colspan: Option<u32>,
-    data: Option<String>,
-) -> Cell {
+pub fn new_cell(x: u32, y: u32, rowspan: u32, colspan: u32, data: String) -> Cell {
     Cell {
         x,
         y,
-        rowspan: match rowspan {
-            Some(a) => a,
-            None => 1,
-        },
+        rowspan,
+        colspan,
 
-        colspan: match colspan {
-            Some(a) => a,
-            None => 1,
-        },
-
-        data: match data {
-            Some(a) => a,
-            None => "".to_string(),
-        },
+        data: data,
     }
 }
 
 pub fn new_row(width: u32, order: u32) -> Row {
     let mut vec_of_cells: Vec<Cell> = Vec::new();
     for x in 0..width {
-        vec_of_cells.push(new_cell(x, order, None, None, None))
+        vec_of_cells.push(new_cell(x, order, 1, 1, "".to_string()))
     }
     Row {
         order,
@@ -165,15 +161,103 @@ mod tests {
 
     #[test]
     fn cell_rust_to_html_test() {
-        let rust_cell = new_cell(1, 1, Some(1), Some(2), Some(String::from("hioh!")));
+        let rust_cell = new_cell(1, 1, 1, 2, String::from("hioh!"));
         let html_cell = "<td x=1 y=1 rowspan=1 colspan=2 ><input>hioh!</input></td>".to_string();
         assert_eq!(rust_cell.to_html(), html_cell);
     }
     #[test]
     fn cell_html_to_rust_test() {
-        let rust_cell = new_cell(1, 2, Some(2), Some(2), Some(String::from("hio222h!")));
+        let rust_cell = new_cell(1, 2, 2, 2, String::from("hio222h!"));
         let html_cell = "<td x=1 y=2 rowspan=2 colspan=2 ><input>hio222h!</input></td>".to_string();
 
-        assert_eq!(Cell::from(html_cell), rust_cell);
+        assert_eq!(Cell::from(&html_cell), rust_cell);
+    }
+    #[test]
+    fn table_parsing_test() {
+        let html_table = String::from("<table><tr><td x=0 y=0 rowspan=1 colspan=1 ><input></input></td><td x=1 y=0 rowspan=1 colspan=1 ><input></input></td><td x=2 y=0 rowspan=1 colspan=1 ><input></input></td></tr><tr><td x=0 y=1 rowspan=1 colspan=1 ><input></input></td><td x=1 y=1 rowspan=1 colspan=1 ><input>hioh</input></td><td x=2 y=1 rowspan=1 colspan=1 ><input></input></td></tr><tr><td x=0 y=2 rowspan=1 colspan=1 ><input></input></td><td x=1 y=2 rowspan=1 colspan=1 ><input>HOH</input></td><td x=2 y=2 rowspan=1 colspan=1 ><input></input></td></tr></table>");
+        let rust_table = Table {
+            rows: Vec::from([
+                Row {
+                    order: 0,
+                    cells: Vec::from([
+                        Cell {
+                            x: 0,
+                            y: 0,
+                            rowspan: 1,
+                            colspan: 1,
+                            data: "".to_string(),
+                        },
+                        Cell {
+                            x: 1,
+                            y: 0,
+                            rowspan: 1,
+                            colspan: 1,
+                            data: "".to_string(),
+                        },
+                        Cell {
+                            x: 2,
+                            y: 0,
+                            rowspan: 1,
+                            colspan: 1,
+                            data: "".to_string(),
+                        },
+                    ]),
+                },
+                Row {
+                    order: 1,
+                    cells: Vec::from([
+                        Cell {
+                            x: 0,
+                            y: 1,
+                            rowspan: 1,
+                            colspan: 1,
+                            data: "".to_string(),
+                        },
+                        Cell {
+                            x: 1,
+                            y: 1,
+                            rowspan: 1,
+                            colspan: 1,
+                            data: "hioh".to_string(),
+                        },
+                        Cell {
+                            x: 2,
+                            y: 1,
+                            rowspan: 1,
+                            colspan: 1,
+                            data: "".to_string(),
+                        },
+                    ]),
+                },
+                Row {
+                    order: 2,
+                    cells: Vec::from([
+                        Cell {
+                            x: 0,
+                            y: 2,
+                            rowspan: 1,
+                            colspan: 1,
+                            data: "".to_string(),
+                        },
+                        Cell {
+                            x: 1,
+                            y: 2,
+                            rowspan: 1,
+                            colspan: 1,
+                            data: "HOH".to_string(),
+                        },
+                        Cell {
+                            x: 2,
+                            y: 2,
+                            rowspan: 1,
+                            colspan: 1,
+                            data: "".to_string(),
+                        },
+                    ]),
+                },
+            ]),
+        };
+        assert_eq!(html_table, Table::from(&rust_table.to_html()).to_html());
+        assert_eq!(Table::from(&Table::from(&html_table).to_html()), rust_table);
     }
 }
