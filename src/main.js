d17cc1ef07ac9c4200b3af46297b093c2d4208c2
bucholski <bucholskim@gmail.com>
2023-12-15 21:08:36 +0100

    experimenting w/CSS to set the ground for add/remove column/row functionality

--- src/main.js
+++ src/main.js
@@ -3,14 +3,26 @@ const { invoke } = window.__TAURI__.tauri;
 async function generateNewTable(width, height) {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   let table = document.querySelector("#table");
-  table.innerHTML = await invoke("generate_new_table", { height: height.value, width: width.value });
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#table-generation-form").addEventListener("submit", (e) => {
-    let width = document.querySelector("#width-input");
-    let height = document.querySelector("#height-input");
-    e.preventDefault();
-    (width.value && height.value) ? generateNewTable(width, height) : null
+  table.innerHTML = await invoke("generate_new_table", {
+    height: height.value,
+    width: width.value,
   });
+}
+//Debug below
+//Initiate with a 3x3 table
+window.addEventListener("DOMContentLoaded", () => {
+  let width = {};
+  let height = {};
+  width.value = "3";
+  height.value = "3";
+  width.value && height.value ? generateNewTable(width, height) : null;
+  //Debug above
+  document
+    .querySelector("#table-generation-form")
+    .addEventListener("submit", (e) => {
+      let width = document.querySelector("#width-input");
+      let height = document.querySelector("#height-input");
+      e.preventDefault();
+      width.value && height.value ? generateNewTable(width, height) : null;
+    });
 });
